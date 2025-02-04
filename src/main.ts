95085c5be9d6a4f715bf1897e2383c79604e4703
hlw422 <hlw422@126.com>
2025-02-04 09:48:49 +0800

    增加pinia 用户状态持久化

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import router from './router'
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 
+
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(Antd)
 
--- src/stores/useLoginUserStore.ts
+++ src/stores/useLoginUserStore.ts
@@ -2,21 +2,26 @@ import { getLoginUserUsingGet } from "@/api/userController"
 import { defineStore } from "pinia"
 import { ref } from "vue"
 
-export const useCounterStore = defineStore('loginUser', () => {
-    const loginUser = ref<API.LoginUserVO>(
-        {
-            userName: 'not login'
+
+export const useCounterStore = defineStore('loginUser',
+    () => {
+
+        const loginUser = ref<API.LoginUserVO>(
+            {
+                userName: 'not login'
+            }
+        )
+        async function fetchLoginUser() {
+            const res = await getLoginUserUsingGet();
+            if (res.data.code == 0 && res.data.data) {
+                loginUser.value = res.data.data;
+            }
         }
-    )
-    async function fetchLoginUser() {
-        const res = await getLoginUserUsingGet();
-        if (res.data.code == 0 && res.data.data) {
-            loginUser.value = res.data.data;
+        function setLoginUser(user: API.LoginUserVO) {
+            loginUser.value = user;
         }
-    }
-    function setLoginUser(user: API.LoginUserVO) {
-        loginUser.value = user;
-    }
-    return { loginUser, fetchLoginUser ,setLoginUser}
+        return { loginUser, fetchLoginUser, setLoginUser }
+    }, {
+    persist: true  // This will enable localStorage persistence
 })
 
