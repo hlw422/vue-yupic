0bafd59e37001f3fe30c3228bb9713033366df19
hlw422 <hlw422@126.com>
2025-02-26 20:02:49 +0800

    1.图片管理界面增加审核状态筛选 2.增加图片拒绝必须填写原因

--- src/constants/picture.ts
+++ src/constants/picture.ts
@@ -1,12 +1,13 @@
 /**
  * 图片审核枚举
  */
-export const PIC_REVIEW_STATUS_ENUM = {  
-    REVIEWING: 0,  
-    PASS: 1,  
-    REJECT: 2,  
-  }  
-    
+export const PIC_REVIEW_STATUS_ENUM = {
+    ALL: -1,
+    REVIEWING: 0,
+    PASS: 1,
+    REJECT: 2,
+}
+
 // 定义一个类型接口，用于描述 PIC_REVIEW_STATUS_MAP 对象的结构
 interface PicReviewStatusMap {
     [key: number]: string;
@@ -14,15 +15,15 @@ interface PicReviewStatusMap {
 
 // 定义 PIC_REVIEW_STATUS_MAP 常量，并指定其类型为 PicReviewStatusMap
 export const PIC_REVIEW_STATUS_MAP: PicReviewStatusMap = {
+    [-1]: '全部',
     0: '待审核',
     1: '通过',
     2: '拒绝',
 };
-    
-  export const PIC_REVIEW_STATUS_OPTIONS = Object.keys(PIC_REVIEW_STATUS_MAP).map((key:any) => {  
-    return {  
-      label: PIC_REVIEW_STATUS_MAP[key],  
-      value: key,  
-    }  
-  })
-  
+
+export const PIC_REVIEW_STATUS_OPTIONS = Object.keys(PIC_REVIEW_STATUS_MAP).map((key: any) => {
+    return {
+        label: PIC_REVIEW_STATUS_MAP[key],
+        value: key,
+    }
+})
--- src/pages/picture/PictureManagerPage.vue
+++ src/pages/picture/PictureManagerPage.vue
@@ -11,6 +11,10 @@
                 <a-select v-model:value="searchParams.tags" mode="tags" allow-clear placeholder="请输入标签"
                     style="min-width: 180px;"></a-select>
             </a-form-item>
+            <a-form-item name="reviewStatus" label="审核状态">
+                <a-select :options="PIC_REVIEW_STATUS_OPTIONS" v-model:value="searchParams.reviewStatus" allow-clear
+                    placeholder="请选择审核状态" style="min-width: 180px;"></a-select>
+            </a-form-item>
             <a-form-item>
                 <a-button type="primary" html-type="submit">搜索</a-button>
             </a-form-item>
@@ -52,8 +56,10 @@
                 <template v-else-if="column.key === 'action'">
                     <a-space wrap>
                         <a-button type="link" :href="`/add_picture?id=${record.id}`" target="_blank">编辑</a-button>
-                        <a-button v-if="record.reviewStatus != PIC_REVIEW_STATUS_ENUM.PASS" type="link" @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.PASS)">通过</a-button>
-                        <a-button v-if="record.reviewStatus != PIC_REVIEW_STATUS_ENUM.REJECT" type="link" @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.REJECT)">拒绝</a-button>
+                        <a-button v-if="record.reviewStatus != PIC_REVIEW_STATUS_ENUM.PASS" type="link"
+                            @click="handleReview(record, PIC_REVIEW_STATUS_ENUM.PASS)">通过</a-button>
+                        <a-button v-if="record.reviewStatus != PIC_REVIEW_STATUS_ENUM.REJECT" type="link"
+                            @click="openRejectReasonModal(record)">拒绝</a-button>
                         <a-popconfirm title="确认删除该图片吗？" @confirm="() => deletepicture(record.id)" ok-text="确认"
                             cancel-text="取消">
                             <a-button danger>删除</a-button>
@@ -62,15 +68,56 @@
                 </template>
             </template>
         </a-table>
+        <!-- 审核拒绝原因模态框 -->
+        <a-modal v-model:visible="isRejectModalVisible" title="拒绝原因" @ok="handleRejectSubmit"
+            @cancel="handleRejectCancel">
+            <a-form>
+                <a-form-item label="拒绝原因">
+                    <a-input v-model:value="reviewMessage" placeholder="请输入拒绝原因" />
+                </a-form-item>
+            </a-form>
+        </a-modal>
+
     </div>
 </template>
 <script lang="ts" setup>
 import { deletePictureUsingPost, doPictureReviewUsingPost, listPictureByPageUsingPost, listPictureVoByPageUsingPost } from '@/api/pictureController';
-import { PIC_REVIEW_STATUS_ENUM, PIC_REVIEW_STATUS_MAP } from '@/constants/picture';
+import { PIC_REVIEW_STATUS_ENUM, PIC_REVIEW_STATUS_MAP, PIC_REVIEW_STATUS_OPTIONS } from '@/constants/picture';
 import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
 import { message } from 'ant-design-vue';
 import dayjs from 'dayjs';
 import { computed, onMounted, reactive, ref } from 'vue';
+
+
+// 控制拒绝原因模态框的显示状态
+const isRejectModalVisible = ref(false);
+// 当前模态框的审核状态（通过或拒绝）
+const rejectReasonModalStatus = ref(0);
+const rejectReason = ref('');
+
+const reviewMessage=ref('');
+
+const currentPicId = ref<number>(0);
+
+
+const handleRejectCancel = () => {
+    isRejectModalVisible.value = false;
+}
+const openRejectReasonModal = (record: API.Picture) => {
+    isRejectModalVisible.value=true;
+    currentPicId.value = record.id;
+}
+
+/**
+ * 提交审核
+ * @param record 
+ * @param status 
+ */
+const handleRejectSubmit = async (record: API.Picture, status: number) => {
+    handleReview(record, PIC_REVIEW_STATUS_ENUM.REJECT);
+};
+
+
 const columns = [
     {
         title: 'id',
@@ -162,6 +209,7 @@ const doTableChange = (page: any) => {
 const getpictures = async () => {
     const res = await listPictureByPageUsingPost({
         ...searchParams,
+        reviewStatus: searchParams.reviewStatus==-1?"":searchParams.reviewStatus
     })
     if (res.data.code == 0 && res.data.data) {
         dataList.value = res.data.data.records ?? [] as any;
@@ -188,18 +236,26 @@ const doSearch = () => {
  * @param status 
  */
 const handleReview = async (record: API.Picture, status: number) => {
+    if(status == PIC_REVIEW_STATUS_ENUM.REJECT){
+        if(!reviewMessage.value){
+            message.error('请输入拒绝原因');
+            return;
+        }
+    }
     try {
         const res = await doPictureReviewUsingPost({
-            id: record.id,
+            id: record.id??currentPicId.value,
             reviewStatus: status,
-            reviewMessage: status == PIC_REVIEW_STATUS_ENUM.PASS ? '管理员审核通过' : '管理员审核不通过'
+            reviewMessage: status == PIC_REVIEW_STATUS_ENUM.PASS ? '管理员审核通过' : reviewMessage.value,
         })
         getpictures();
         message.success('审核成功');
     }
-    catch (e:any) {
+    catch (e: any) {
         message.error('审核失败：' + e.message);
     }
+    reviewMessage.value='';
+    isRejectModalVisible.value=false;
 }
 
 const deletepicture = async (id: number) => {
